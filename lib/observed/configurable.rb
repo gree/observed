a374acf22a9d1437f93eeb9139cb1de82bf23082
KUOKA Yusuke <yusuke.kuoka@gree.net>
2013-10-29 10:39:48 +0900

    Options for Observed::Configuration.attribute to define attributes along with default values

--- lib/observed/configurable.rb
+++ lib/observed/configurable.rb
@@ -1,12 +1,26 @@
 module Observed
   module Configurable
 
+    def initialize(args={})
+      configure(args)
+    end
+
+    def configure(args={})
+      if @attributes
+        @attributes.merge! args
+      else
+        @attributes ||= args.dup
+      end
+    end
+
     module ClassMethods
       # @param [Symbol] name
-      def attribute(name)
+      def attribute(name, options={})
         define_method(name) do
           instance_variable_get("@#{name.to_s}") || @attributes[name] || self.class.defaults[name] || fail_because_of_non_configured_parameter(name)
         end
+        default_value =  options && options[:default]
+        default name => default_value if default_value
       end
 
       def default(args)
@@ -25,22 +39,6 @@ module Observed
 
     class NotConfiguredError < RuntimeError; end
 
-    def initialize(args={})
-      configure(args)
-    end
-
-    def configure(args={})
-      if @attributes
-        @attributes.merge! args
-      else
-        @attributes ||= args.dup
-      end
-    end
-
-    def default_value_for(name)
-      self.class.defaults[name]
-    end
-
     class << self
       def included(klass)
         klass.extend ClassMethods
@@ -48,6 +46,10 @@ module Observed
     end
 
     private
+
+    def default_value_for(name)
+      self.class.defaults[name]
+    end
 
     def fail_because_of_non_configured_parameter(name)
       fail NotConfiguredError.new("The parameter `#{name}` is not configured.")
---
+++ spec/configurable_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+require 'observed/configurable'
+
+module ConfigurableSpec
+  class Foo
+    include Observed::Configurable
+
+    attribute :foo, :default => 123
+    attribute :bar, :default => 234
+    attribute :baz
+
+    default :bar => 345
+  end
+end
+
+describe Observed::Configurable do
+
+  context 'without parameters for the constructor' do
+    subject {
+      ConfigurableSpec::Foo.new
+    }
+    it 'uses default values for attributes' do
+      expect(subject.foo).to eq(123)
+    end
+    it 'overrides default values on `attribute name, :default => default_value`' do
+      expect(subject.bar).to eq(345)
+    end
+    it 'raises errors when attributes without values are read' do
+      expect { subject.baz }.to raise_error
+    end
+  end
+
+  context 'with parameters for the constructor' do
+    subject {
+      ConfigurableSpec::Foo.new({:foo => 1, :bar => 2, :baz => 3})
+    }
+    it 'prefers values from constructor parameters over defaults' do
+      expect(subject.foo).to eq(1)
+      expect(subject.bar).to eq(2)
+      expect(subject.baz).to eq(3)
+    end
+  end
+
+  context 'configured through `configure(args)` method' do
+    subject {
+      foo = ConfigurableSpec::Foo.new
+      foo.configure({:foo => 1, :bar => 2, :baz => 3})
+      foo
+    }
+    it 'prefers values from `configure(args)` over defaults' do
+      expect(subject.foo).to eq(1)
+      expect(subject.bar).to eq(2)
+      expect(subject.baz).to eq(3)
+    end
+  end
+
+end
