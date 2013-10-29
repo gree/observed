2e67e909050c113377128a24de367119fe2fe2d0
KUOKA Yusuke <yusuke.kuoka@gree.net>
2013-10-29 17:06:13 +0900

    More comments

--- lib/observed.rb
+++ lib/observed.rb
@@ -2,6 +2,22 @@ require 'observed/version'
 require 'observed/config_dsl'
 require 'forwardable'
 
+# The module to provide DSL to describe Observed configuration, intended to be used by including to Ruby's `main` object
+# like Clockwork(https://github.com/tomykaira/clockwork) does in their configuration file(a.k.a `clockwork.rb`).
+#
+# Take this as the `Builder` for Observed's configuration which is has global state.
+# As it has global state, we have to call `Observed#init!` before building multiple Observed configurations through
+# this module.
+#
+# @example
+# require 'observed'
+# include Observed
+#
+# configure { plugins_directory: 'plugins' }
+# observe 'myservice.response', { plugin: 'http', method: 'get', url: 'http://localhost:3000' }
+# match 'myservice.response', { plugin: 'stdout' }
+#
+# #=> Now we can obtain the described configuration by calling `Observed.config`
 module Observed
   extend self
   extend Forwardable
@@ -10,6 +26,9 @@ module Observed
 
   def_delegators :@@observed, :require, :observe, :config, :load!
 
+  # Call this method before you are going to build 2nd or later Observed configuration using this module.
+  # Refrain that `Observed` object is a builder for Observed configuration and it has global state.
+  # We have to reset its state via this `init!` method before building next configurations after the first one.
   def init!
     @@observed = Observed::ConfigDSL.new
   end
--- lib/observed/config_dsl.rb
+++ lib/observed/config_dsl.rb
@@ -1,6 +1,9 @@
 require 'observed/input_plugin'
 
 module Observed
+  # The DSL to describe Observed's configuration.
+  # @example
+  # ConfigDSL.new.instance_eval user_code_from_configuration_file
   class ConfigDSL
 
     alias original_require require
@@ -15,6 +18,9 @@ module Observed
       end
     end
 
+    # We want to require Ruby scripts reside in the same directory of `observed.conf` a.k.a Observed's configuration.
+    # For that, we have to mutate Ruby's LOAD_PATH but we also prefer mutating it locally in this method over
+    # mutating it globally.
     def require(lib)
       $LOAD_PATH.push plugins_directory.to_s
       #original_require "#{plugins_directory + lib}"
@@ -22,14 +28,21 @@ module Observed
       $LOAD_PATH.delete plugins_directory.to_s
     end
 
-    def observe(observation_name, observation)
-      if inputs[observation_name]
-        fail "An observation named '#{observation_name}' already exists."
+    # @param [String] tag The tag which is assigned to data which is generated from this input and is sent to output
+    # later
+    # @param [Hash] input The configuration for each input conatining (1) which input plugin to use for this input and
+    # (2) which parameters to pass to input plugin.
+    def observe(tag, input)
+      if inputs[tag]
+        fail "An observation named '#{tag}' already exists."
       else
-        inputs[observation_name] = observation
+        inputs[tag] = input
       end
     end
 
+    # @param [Regexp] tag The pattern to match inputs' tags
+    # @param [Hash] output The configuration for each output containing (1) which output plugin to use for this output
+    # and (2) which parameters to pass to the output plugin
     def match(tag, output)
       if outputs[tag]
         fail "A `match` for the tag '#{tag}' already exists."
@@ -42,6 +55,9 @@ module Observed
       { :inputs => inputs, :outputs => outputs }
     end
 
+    # Load the file and evalute the containing code in context of this object(a.k.a DSL).
+    # @param [String|Pathname] file The path to Ruby script containing the code in Observed's configuration DSL,
+    # typically `observed.conf`.
     def load!(file)
       code = File.read(file)
       instance_eval code, file
