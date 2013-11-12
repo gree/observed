660336de565957d78352b85507f1809ba1d8ab4e
KUOKA Yusuke <yusuke.kuoka@gree.net>
2013-11-12 19:47:23 +0900

    Add an example code to show concurrency achieved by using observed-eventmachine

---
+++ integrations/observed-eventmachine/examples/observed.rb
@@ -0,0 +1,30 @@
+require 'observed'
+require 'observed/eventmachine'
+
+include Observed
+
+# This is a observed-eventmachine specific code to extend the DSL to use EventMachine
+extend Observed::EM
+
+class Test < Observed::Observer
+  plugin_name 'test'
+  def observe
+    system.report(tag, {foo:1})
+    puts "Sleeping 10 seconds"
+    sleep 10.0
+    puts "Slept 10 seconds"
+  end
+end
+
+observe 'foo', via: 'test'
+
+report /foo/, via: 'stdout', with: {
+  format: -> _, _, data { "#{data}" }
+}
+
+# This is a observed-eventmachine specific code to schedule the observation on 'foo'
+every 1, run: 'foo'
+
+# This is a observed-eventmachine specific code to start running observations periodically, until we stop that by
+# sending signals
+start
