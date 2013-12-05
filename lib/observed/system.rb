d87d73daf312d689281b9d3363c9c10ee583ab0e
KUOKA Yusuke <yusuke.kuoka@gree.net>
2013-12-05 19:05:27 +0900

    Remove the unused class Observed::YAML

--- lib/observed/system.rb
+++ lib/observed/system.rb
@@ -66,46 +66,4 @@ module Observed
 
   end
 
-  class YAML
-    def observers
-      config.observers
-      @observers ||= begin
-
-        observer_configs = config.observers
-
-        observers = {}
-
-        observer_configs.each do |tag, input_config|
-          plugin_name = input_config[:plugin] || fail(RuntimeError, %Q|Missing plugin name for the tag "#{tag}" in "#{input_config}" in "#{config}".|)
-          plugin = observer_plugins[plugin_name] || fail(RuntimeError, %Q|The plugin named "#{plugin_name}" is not found in plugins list "#{observer_plugins}".|)
-          updated_config = input_config.merge(tag: tag)
-          observer = plugin.new(updated_config)
-          observer.configure(system: self, logger: logger)
-          observers[tag] = observer
-        end
-
-        observers
-      end
-    end
-
-    def reporters
-      @reporters ||= begin
-
-        reporter_configs = config.reporters
-
-        reporters = {}
-
-        reporter_configs.each do |tag_pattern, output_config|
-          plugin_name = output_config[:plugin] || fail(RuntimeError, %Q|Missing plugin name for the output for "#{tag_pattern}" in "#{output_config}" in #{config}.|)
-          plugin = reporter_plugins[plugin_name] || fail(RuntimeError, %Q|The plugin named "#{plugin_name}" is not found in plugins list "#{reporter_plugins}".|)
-          updated_config = output_config.merge(tag_pattern: Regexp.new(tag_pattern))
-          reporter = plugin.new(updated_config)
-          reporter.configure(system: self, logger: logger)
-          reporters[tag_pattern] = reporter
-        end
-
-        reporters
-      end
-    end
-  end
 end
